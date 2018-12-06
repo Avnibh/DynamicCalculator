a45248c262d624949e40d005b687be619eb82982
Pankaj <whenitis19@gmail.com>
2018-12-06 18:47:05 +0530

    Fourth Commit

--- README.txt
+++
@@ -1,3 +0,0 @@
-A Pen created at CodePen.io. You can find this one at https://codepen.io/pankajp27/pen/qQzzjV.
-
- The coolest auto loan calculator you'll ever see!
--- js/index.js
+++ js/index.js
@@ -6,8 +6,34 @@ function myFunction() {
         cgst1 = cgst1/2,
         sgst1 = sgst1/2,
         price1 = parseInt(totalPrice),
-    
-        calculation = (price1*cgst1)/100 +(price1*sgst1)/100;
+        cgst1 = (price1*cgst1)/100
+        sgst1 = (price1*sgst1)/100
+        igst =  cgst1 + sgst1,
+        allTotal = price1 + igst;
   
-    document.getElementById("output").innerHTML = calculation;
+
+    document.getElementById("output1").innerHTML = cgst1;
+    document.getElementById("output2").innerHTML = sgst1;
+    document.getElementById("output3").innerHTML = igst;
+    document.getElementById("output4").innerHTML = allTotal;
 }
+
+$('#e12').click(function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    $(this).toggleClass('expanded');
+    $('#'+$(e.target).attr('for')).prop('checked',true);
+});
+$(document).click(function() {
+    $('.dropdown-el').removeClass('expanded');
+});
+
+$('#e11').click(function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    $(this).toggleClass('expanded');
+    $('#'+$(e.target).attr('for')).prop('checked',true);
+});
+$(document).click(function() {
+    $('.dropdown-el').removeClass('expanded');
+});
