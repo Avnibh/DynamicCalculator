31f3b84cd5ca67696fbcb80428efed6972e548c9
Pankaj M. Patidar <39170113+imPatidar@users.noreply.github.com>
2018-12-09 23:31:08 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -30,12 +30,12 @@ function myFunction() {
 }
 
 
-$('.dropdown-el').click(function(e) {
-    e.preventDefault();
-    e.stopPropagation();
-    $(this).toggleClass('expanded');
-    $('#'+$(e.target).attr('for')).prop('checked',true);
-});
-$(document).click(function() {
-    $('.dropdown-el').removeClass('expanded');
-});
+// $('.dropdown-el').click(function(e) {
+//     e.preventDefault();
+//     e.stopPropagation();
+//     $(this).toggleClass('expanded');
+//     $('#'+$(e.target).attr('for')).prop('checked',true);
+// });
+// $(document).click(function() {
+//     $('.dropdown-el').removeClass('expanded');
+// });
